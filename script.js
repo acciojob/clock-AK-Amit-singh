54de841a2728e74da62cbc280915ce42aa308947
Amit Singh <aksingh0518@gmail.com>
2026-08-08 16:14:28 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 //your JS code here. If required.
+function updateTimer() {
+    const timer = document.getElementById("timer");
+
+    const now = new Date();
+
+    timer.textContent = now.toLocaleString();
+}
+
+// Display immediately
+updateTimer();
+
+// Update every second
+setInterval(updateTimer, 1000);
